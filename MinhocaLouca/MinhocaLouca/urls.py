c3d81fc9e79b645ade79774a3b8aa2f5e42ee3da
MatheusPK <matheuskulick@hotmail.com>
2022-11-16 22:55:06 -0300

    [ADD] Old project to django setup

--- MinhocaLouca/MinhocaLouca/urls.py
+++ MinhocaLouca/MinhocaLouca/urls.py
@@ -17,5 +17,6 @@ from django.urls import path
 from MinhocaLoucaApp import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('game', views.game, name='game') 
 ]
