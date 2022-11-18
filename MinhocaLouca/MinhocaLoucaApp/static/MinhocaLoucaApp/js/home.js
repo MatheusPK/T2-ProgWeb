e70f188efa9809910e0daa4b5fb2d97388fe19c1
TheoCaldas <theocaldas28@gmail.com>
2022-11-17 21:49:12 -0300

    [FIX] Difficulty not selected

--- MinhocaLouca/MinhocaLoucaApp/static/MinhocaLoucaApp/js/home.js
+++ MinhocaLouca/MinhocaLoucaApp/static/MinhocaLoucaApp/js/home.js
@@ -1,38 +1,9 @@
-var fieldToMessageID = { //global
-    "emailField": "emailErrorMessage",
-    "difficultyField": "difficultyErrorMessage"
-}
-
 onload = function(){
-    document.getElementById("emailField").addEventListener("input", hideError);
     document.getElementById("difficultyField").addEventListener("input", hideError);
 }
 
-//returns if form input is correct
-function validateForm(){
-    var isValidEmail = checkEmail();
-    var isDiffSelected = checkDifficulty();
-
-    return (isValidEmail && isDiffSelected);
-}
-
-//returns if typed email is in the correct format
-function checkEmail(){
-    var emailElement = document.getElementById("emailField");
-    var typedEmail = emailElement.value;
-
-    // var re = /^\\[^aeiou]+\[[^aeiou]+\|([^aeiou]+\|)*[^aeiou]+\]$/;
-    var re = /^\\[qwrtypsdfghjklzxcvbnm]+\[[qwrtypsdfghjklzxcvbnm]+\|([qwrtypsdfghjklzxcvbnm]+\|)*[qwrtypsdfghjklzxcvbnm]+\]$/;
-
-    if (!re.test(typedEmail.toLowerCase())){
-        showError(emailElement);
-        return false;
-    }
-    return true;
-}
-
 //returns if game difficulty is selected
-function checkDifficulty(){
+function validateForm(){
     var diffElement = document.getElementById("difficultyField");
     var selectedDiff = diffElement.value;
 
@@ -46,11 +17,11 @@ function checkDifficulty(){
 //resets event target style and hide associated error message
 function hideError(event){
     event.target.style.outline = "none";
-    document.getElementById(fieldToMessageID[event.target.id]).style.visibility = "hidden";
+    document.getElementById("difficultyErrorMessage").style.visibility = "hidden";
 }
 
 //change element style and show associated error message
 function showError(element){
-    element.style.outline = "3px solid red";
-    document.getElementById(fieldToMessageID[element.id]).style.visibility = "visible";
+    element.style.outline = "3px solid rgb(135, 2, 2)";
+    document.getElementById("difficultyErrorMessage").style.visibility = "visible";
 }
